a612d5b59b91e9d85dd8d055d24c77d821d729f2
Szymon <szymonczaja2003@gmail.com>
2026-06-03 21:31:31 +0200

    vector_store.py: query_collection func

--- app/services/vector_store.py
+++ app/services/vector_store.py
@@ -1,8 +1,27 @@
 import chromadb
 from app.core.config import settings
+import uuid
+from typing import Dict
+
+if not settings.CHROMA_PATH.exists():
+    settings.CHROMA_PATH.mkdir(parents=True, exist_ok=True)
 
 client = chromadb.PersistentClient(path=settings.CHROMA_PATH)
 collection = client.get_or_create_collection(
-    name='uni-bip', 
-    metadata={'hnsw:space': 'cosine'}
+name='uni-bip', 
+metadata={'hnsw:space': 'cosine'}
 )
+
+def add_documents(chunks : list, vectors : list, metadatas : list):
+    collection.add(
+        ids= [uuid.uuid4().hex for x in chunks],
+        embeddings=vectors, 
+        documents=chunks,
+        metadatas=metadatas)
+    
+def query_collecion(question_embedding : list, top_k : int) -> Dict[str, list]: 
+    results = collection.query(
+        query_embeddings=question_embedding, 
+        n_results=top_k
+    )
+    return results
